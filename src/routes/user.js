1296a161111aff63713b3c7655380e566f83747f
Ramen Kar <109908494+Kar1234Ramen@users.noreply.github.com>
2026-01-04 21:07:39 +0530

    fixed issue -get connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -30,7 +30,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
     const connectionRequest = await ConnectionRequest.find({
       $or: [
-        { toUserId: loggedInUser._id, status: "acccepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
     })
